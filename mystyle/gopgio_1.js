93f9e0987b0705f758543ae646bb921c3b335502
minhman10410 <14520511@gm.uit.edu.vn>
2017-04-18 09:41:28 +0700

    1

---
+++ mystyle/gopgio_1.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+// --------------------------------------------------------------------
+// --------------------------------------------------------------------
+//SlideShow
+/*
+var slidePhoto = document.getElementById("slideImg");
+var index = 0;
+var arrayImg = ["img/slide2.jpg", "img/slide3.jpg", "img/slide4.jpg", "img/slide5.jpg", "img/slide6.jpg", "img/slide1.jpg"]
+
+function imgChange()
+{
+	slideImg.setAttribute("src", arrayImg[index]);
+	index++;
+	if(index > arrayImg.length - 1)
+		index = 0;
+	}
+	
+var intervalHandle = setInterval(imgChange, 3000);
+slideImg.onclick = function()
+{
+	clearInterval(intervalHandle);
+	}
+*/
